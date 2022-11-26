617210eae87ed038e87c54b810b23a58f50df132
Vuong Dang <vuongdang@chidoanh.com>
2022-11-26 21:29:37 +0700

    udpate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { Box } from './components/Box'
 import './assets/css/taildwin.css'
-import { Count } from './components/Count'
-import { Form } from './components/Form'
 
 function App() {
-  const [isRed, setIsRed] = useState(true)
 
   const attri = {
     background: 'red',
@@ -15,18 +12,9 @@ function App() {
 
   return (
     <>
-      {/* <Box {...attri}/>
+      <Box {...attri}/>
       <Box background="green" text={1 + 10 + 20}/>
-<<<<<<< HEAD
-      <Box background="brown" text={324234} /> */}
-      {/* <Count /> */}
-      <Form />
-=======
-      <Box background="brown" text={<bold>324234</bold>} /> */}
-      <button onClick={() => setIsRed(!isRed)}>toggle</button>
-      <Count count={10} background={isRed ? 'red': 'green'}/>
-      {/* <Form /> */}
->>>>>>> e1ecd69f716f0b39adae58c5f4203f284b82ca62
+      <Box background="brown" text={324234} />
     </>
   )
 }
