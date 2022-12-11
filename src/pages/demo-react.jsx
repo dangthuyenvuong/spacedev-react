6f73b7ceda97f1687a183339c36971e4f45db02a
Vuong Dang <vuongdang@chidoanh.com>
2022-12-11 17:25:10 +0700

    update

--- src/pages/demo-react.jsx
+++ src/pages/demo-react.jsx
@@ -1,30 +1,28 @@
 import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { useRef } from 'react'
-import Button from '../components/Button'
-import { Input } from '../components/Input';
+import { useId } from 'react'
+import { useRef, useEffect, useState } from 'react'
+import { Input } from '../components/Input'
+
+/**
+ * Cach 1: Lưu trữ giá trị không bị thay đổi mỗi lần component render
+ * 
+ * Cach 2: Selector HTML DOM
+ * 
+ * Cach 3: forwardRef
+ * 
+ * Cach 4: forwardRef va useImperativeHandle -> trả ra 1 thể hiện khác của ref
+ */
 
 export const DemoReact = () => {
-    const ref = useRef(0)
-    const firstInputRef = useRef()
-    const [,setRandom] = useState()
-
+    const inputPhoneId = useId()
+    const inputEmailId = useId()
+    
+    const inputRef = useRef()
     useEffect(() => {
-        setInterval(() => {
-            console.log(ref.current);
-            ref.current++
-        }, 1000)
+        inputRef.current.setValue('Dang Thuyen Vuong')
     }, [])
 
-    useEffect(() => {
-        firstInputRef.current.focus()
-    }, [])
-    const onSubmit = () => {
-        const formData = new FormData(ref.current);
-        const formProps = Object.fromEntries(formData);
-        console.log(formProps);
-    }
+    console.log(inputPhoneId, inputEmailId)
     return (
         <main className="register-course" id="main">
             <section className="section-1 wrap container">
@@ -34,59 +32,48 @@ export const DemoReact = () => {
                     Đừng ngần ngại liên hệ với <strong>Spacedev</strong> để cùng nhau tạo ra những sản phẩm giá trị, cũng như
                     việc hợp tác với các đối tác tuyển dụng và công ty trong và ngoài nước.
                 </p>
-                <form className="form" onSubmit={onSubmit}>
+                <form className="form">
                     <label>
                         <p>Họ và tên<span>*</span></p>
-                        <input type="text" name="name" placeholder="Họ và tên bạn" />
+                        <Input ref={inputRef} type="text" placeholder="Họ và tên bạn" />
                     </label>
                     <label>
                         <p>Số điện thoại</p>
-                        {/* <input type="text" name="phone" placeholder="Số điện thoại" /> */}
-                        <Input ref={firstInputRef} name="phone" placeholder="Số điện thoại" />
+                        <input id={inputPhoneId} type="text" placeholder="Số điện thoại" />
                     </label>
                     <label>
                         <p>Email<span>*</span></p>
-                        <input type="text" name="email" placeholder="Email của bạn" />
+                        <input id={inputEmailId} type="text" placeholder="Email của bạn" />
                     </label>
                     <label>
                         <p>Website</p>
-                        <input type="text" name="website" placeholder="Đường dẫn website http://" />
+                        <input type="text" placeholder="Đường dẫn website http://" />
                     </label>
                     <label>
                         <p>Tiêu đề<span>*</span></p>
-                        <input type="text" name="title" placeholder="Tiêu đề liên hệ" />
+                        <input type="text" placeholder="Tiêu đề liên hệ" />
                     </label>
                     <label>
                         <p>Nội dung<span>*</span></p>
-                        <textarea name="content" id cols={30} rows={10} defaultValue={""} />
+                        <textarea name id cols={30} rows={10} defaultValue={""} />
                     </label>
+                    <button className="btn main rect">đăng ký</button>
+                    {/* COUNT: {count} */}
+                    <br />
+                    <br />
                 </form>
-
-                <Button className="btn main rect" onClick={onSubmit}>đăng ký</Button>
-                COUNT: {ref.current}
-                <Button className="btn main rect" onClick={() => setRandom({})}>Update UI</Button>
             </section>
+            {/* <div class="register-success">
+            <div class="contain">
+                <div class="main-title">đăng ký thành công</div>
+                <p>
+                    <strong>Chào mừng Vương Đặng đã trở thành thành viên mới của Spacedev Team.</strong> <br>
+                    Cảm ơn bạn đã đăng ký khóa học tại <strong>Spacedev</strong>, chúng tôi sẽ chủ động liên lạc với bạn thông qua facebook
+                    hoặc số điện thoại của bạn.
+                </p>
+            </div>
+            <a href="/" class="btn main rect">về trang chủ</a>
+        </div> */}
         </main>
     )
 }
-
-
-
-
-
-/**
- * # Ref level 1:
- * - Thường dùng để lấy ra 1 web-component ---> đặt ref vào trong 1 web component muốn lấy
- * - Xem như 1 biến bình thường lưu trữ giá trị, không bị tạo mới mỗi lần re-render ---> Thay đổi ref.current mà không làm re-render component
- * -
- * 
- * # Ref level 2: forwardRef
- * - Làm sao để lấy 1 web component bên trong 1 component ?
- * - Truyền ref vào 1 component, component phải được bọc trong forwardRef, từ component đó sẽ đặt ref vào trong 1 web component mong muốn
- * 
- * # Ref level 3: forwardRef kết hợp userImperativeHandle ---> Trả ra một thể hiện khác của ref
- * - forwardRef vào trong một component, component sẽ sử dụng useImperativeHandle để trả về cho ref những giá trị cần thiết
- */
-
-
-
