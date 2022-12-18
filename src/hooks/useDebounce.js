bf05e357a8371c2a60f8516ccffb9606d08fbd10
Vuong Dang <vuongdang@chidoanh.com>
2022-12-18 14:51:38 +0700

    update

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,19 @@
+import { useRef } from "react"
+import { useState } from "react"
+
+export const useDebounce = (defaultValue, timing = 300) => {
+    const timingRef = useRef()
+    const [value, _setValue] = useState(defaultValue)
+    
+    const setValue = (value) => {
+        if(timingRef.current) {
+            clearTimeout(timingRef.current)
+        }
+        timingRef.current = setTimeout(() => {
+            _setValue(value)
+            timingRef.current = null
+        }, timing)
+    }
+
+    return [value,setValue]
+}
