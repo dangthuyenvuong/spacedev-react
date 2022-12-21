90dc17c219c311e6a3c3f57a89966f7d2903719e
Vuong Dang <vuongdang@chidoanh.com>
2022-12-21 12:53:15 +0700

    update

--- src/stores/index.js
+++ src/stores/index.js
@@ -29,21 +29,3 @@ export const store = createStore(reducers)
 // setInterval(() => {
 //     store.dispatch({ type: DECREMENT_ACTION, payload: 1.5 })
 // }, 2500)
-
-
-/**
- * store: redux store dùng để quản lý global state
- * 
- * subscribe: Lắng nghe sự thay đổi của state, khi state thay đổi redux sẽ thực thi hàm callback
- * dispatch: Hàm trigger 1 event, input là 1 plan object theo quy định: 
- *      type: Tên của action
- *      payload: data nếu có
- * 
- * 1 store duy nhất, nhiều reducer, mỗi reducer chỉ quản lý những dữ liệu liên quan reducer đó
- * 
- * function reducer: 
- *  Mỗi reducer sẽ quản lý 1 phần dữ liệu của store
- *  Khi dispatch, tất cả reducer sẽ được kích hoạt
- *  reducer cần return về một giá trị, giá trị sẽ được cập nhật vào store. Trong trường hợp return về state cũ, redux sẽ không cần re-render lại
- *  State là plan object, không lưu function
- */
